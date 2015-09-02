fde12e55821265e6c91f65a93a7ff8455a6eb96b
bng11cal <brian.ng8828@gmail.com>
2015-09-02 15:54:39 -0700

    fixed resizing of analyser when browser is resized

--- js/music.js
+++ js/music.js
@@ -28,7 +28,7 @@ var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, b
 
 // Initialize the MP3 player after the page loads all of its HTML into the window
 window.addEventListener("load", initMp3Player, false);
-function initMp3Player(){
+function initMp3Player() {
 	context = new webkitAudioContext(); // AudioContext object instance
 	analyser = context.createAnalyser(); // AnalyserNode method
 	canvas = document.getElementById('analyser_render');
@@ -43,6 +43,7 @@ function initMp3Player(){
 // frameLooper() animates any style of graphics you wish to the audio frequency
 // Looping at the default frame rate that the browser provides(approx. 60 FPS)
 function frameLooper(){
+    canvas.style.width = window.innerWidth - 258 + "px";
 	window.webkitRequestAnimationFrame(frameLooper);
 	fbc_array = new Uint8Array(analyser.frequencyBinCount);
 	analyser.getByteFrequencyData(fbc_array);
