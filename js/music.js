9ec45eb676a4e0d1c09d261b6f9b86e825353564
bng11cal <brian.ng8828@gmail.com>
2015-09-02 17:03:54 -0700

    sharpened edges of bars in analyser

--- js/music.js
+++ js/music.js
@@ -32,7 +32,6 @@ function initMp3Player() {
 	context = new webkitAudioContext(); // AudioContext object instance
 	analyser = context.createAnalyser(); // AnalyserNode method
 	canvas = document.getElementById('analyser_render');
-    canvas.style.width = window.innerWidth - 258 + "px";
 	ctx = canvas.getContext('2d');
 	// Re-route audio playback into the processing graph of the AudioContext
 	source = context.createMediaElementSource(music); 
@@ -43,17 +42,18 @@ function initMp3Player() {
 // frameLooper() animates any style of graphics you wish to the audio frequency
 // Looping at the default frame rate that the browser provides(approx. 60 FPS)
 function frameLooper(){
-    canvas.style.width = window.innerWidth - 258 + "px";
-	window.webkitRequestAnimationFrame(frameLooper);
+    canvas.width = window.innerWidth - 258;
+    window.webkitRequestAnimationFrame(frameLooper);
 	fbc_array = new Uint8Array(analyser.frequencyBinCount);
 	analyser.getByteFrequencyData(fbc_array);
 	ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
 	ctx.fillStyle = '#ffffff'; // Color of the bars
 	bars = 100;
+    var bar_gap = 50;
 	for (var i = 0; i < bars; i++) {
-		bar_x = i * 3;
-		bar_width = 2;
-		bar_height = -(fbc_array[i] / 2);
+		bar_x = i * canvas.width/bars;
+		bar_width = canvas.width/150;
+		bar_height = -(fbc_array[i] / 3);
 		//  fillRect( x, y, width, height ) // Explanation of the parameters below
 		ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
 	}
